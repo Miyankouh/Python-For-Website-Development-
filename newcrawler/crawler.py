6aeca67714e0dc37c1b60e11d5dbb042d013832f
Miyankouh <majid.miyankouh@gmail.com>
2022-05-04 11:48:26 +0430

    Read and write a database using ORM

--- newcrawler/crawler.py
+++ newcrawler/crawler.py
@@ -1,15 +1,19 @@
-import requests
+from models import Article
 from bs4 import BeautifulSoup
+import requests
 
 
 def crawl_page(url):
+    print(url)
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         header_div = soup.find('div', attrs={'class': 'news-detail-header'})
         title = header_div.find('h1')
-        print(title.text)
+        body = soup.select_one('.detYazi cf')
+        return {'body': body.text, 'title': title.text}
+    return {'body':'' , 'title': ''}
 
 
 def get_links():
@@ -23,17 +27,3 @@ def get_links():
                 links.append('https://www.trthaber.com/' + href)
 
     return links
-
-
-if __name__ == "__main__":
-    # links = [
-    #     'https://www.trthaber.com/haber/spor/fenerbahce-beko-rusyada-farkli-kazandi-551690.html',
-    #     'https://www.trthaber.com/haber/spor/bright-osayi-samuel-ilk-kez-takimla-antrenmana-cikti-551726.html',
-    #     'https://www.trthaber.com/haber/spor/galatasaray-mustafa-muhammedin-transferinde-sona-geldi-551713.html',
-    #     'https://www.trthaber.com/haber/spor/eskisehirspor-transfer-yasagi-sorununu-cozemedi-551694.html',
-    # ]
-    #
-    # for link in links:
-    #     crawl_page(link)
-
-    get_links()
--- newcrawler/main.py
+++ newcrawler/main.py
@@ -1,21 +1,48 @@
 import sys
+from crawler import crawl_page
 from utils.db import create_tables
 from crawler import get_links
 
 from models import Article, Category
 
 
-def run():
-    links = get_links()
-
+def crawl_and_get_links():
     cat = Category.create(name='Sport')
-    for link in links:
+    for link in get_links():
         article = Article.create(url=link, category=cat)
         print(article.id)
 
 
+def crawl_and_scrape_articles():
+    articles = Article.select().where(Article.is_completed == False)
+    
+    for article in articles:
+        try:
+            data = crawl_page(article.url)
+        except:
+            article.is_completed = True
+            article.save()
+        else:
+            article.title = data['title']
+            article.body = data['body']
+            article.is_completed = True
+            article.save()
+
+
+def show_stats():
+    articles = Article.select().count()
+    categories = Category.select().count()
+    completed = Article.select().where(Article.is_completed==True).count()
+    print(f"articles: {articles}\t categories: {categories}\t completed: {completed}")
+
+
 if __name__ == '__main__':
+
     if sys.argv[1] == 'create_tables':
         create_tables()
-    elif sys.argv[1] == 'run':
-        run()
+    elif sys.argv[1] == 'get_links':
+        crawl_and_get_links()
+    elif sys.argv[1] == 'get_article':
+        crawl_and_scrape_articles()
+    elif sys.argv[1] == 'stats':
+        show_stats()
