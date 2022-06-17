d9205a0e5602c0f9ee1693e65388590e71605885
miyankouh <majid.miyankouh@gmail.com>
2022-06-17 19:12:53 +0430

    add: catalogue & blog & academy & gitignore

--- academy/blog/urls.py
+++ academy/blog/urls.py
@@ -1,1 +1,22 @@
+from django.urls import path, re_path, register_converter
+from blog.utils import FourDigitYear
+from blog.views import category_list, post_detail,  post_list
 
+
+register_converter(FourDigitYear, 'fourdigit')
+
+
+urlpatterns = [
+    path("list/", post_list),
+    path("detail/<int:post_id>/", post_detail ),
+    path("detail/<uuid:post_uuid>/", post_detail ),
+    path("detail/<slug:post_slug>/", post_detail ),
+    re_path(r"detail/(?P<post_slug>)[\w-])+/", post_detail),
+    path("categories/test/", category_list),
+    path("archive/<fourdigit:year>/", post_list),
+    re_path(r"archive/(?P<year>[0-9]{2.4})", post_list),
+    path("archive/<int:year>/<int:month>/", post_list),
+
+    re_path(r"archive/(?P<code>[0-9]{4})/", post_list),
+    re_path(r"archive/(?P<code>[0-9]{6})/", post_list),
+]
--- academy/blog/views.py
+++ academy/blog/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request, year=None, month=None):
+    if month is not None:
+        return HttpResponse(f'post list archive for {year} and {month}')
+        
+    if year is not None:
+        return HttpResponse(f'post list archive for {year}')
+    
+    
+    return HttpResponse('posts list page')
+    
+
+def category_list(request):
+    return HttpResponse('category list page')
+    
+
+def post_detail(request, post_title):
+    return HttpResponse(f'post detail {post_title}')
