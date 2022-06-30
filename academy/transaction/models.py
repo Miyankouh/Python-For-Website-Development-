411b689681a65a0193e08b564c695708110efcd9
miyankouh <majid.miyankouh@gmail.com>
2022-06-30 14:01:30 +0430

    add: get_report & get_total_balance

--- academy/transaction/models.py
+++ academy/transaction/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Count, Sum, Q
+from django.db.models.functions import Coalesce
 
 
 class Transaction(models.Model):
@@ -13,14 +15,38 @@ class Transaction(models.Model):
         (TRANSFER, "Transfer"),
     )
     user = models.ForeignKey(
-        User, related_name='transaction', on_delete=models.RESTRICT)
+        User, related_name='transactions', on_delete=models.RESTRICT)
     transaction_type = models.PositiveSmallIntegerField(
         choices=TRANSACTION_TYPE_CHOICES, default=CHARGE)
     amount = models.BigIntegerField()
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} - {self.get_transaction_type_display()} - {self.amount}"
+        return f"{self.user} - {self.get_transaction_type_display()} - {self.amount}"
+
+    @classmethod
+    def get_report(cls):
+        """ show all users and their balance """
+        positive_transactions = Sum(
+            'transactions__amount',
+            filter=Q(transactions__transaction_type=1)
+        )
+
+        negative_transactions = Sum(
+            'transactions__amount',
+            filter=Q(transactions__transaction_type__in=[2, 3])
+        )
+        
+        users = User.objects.all().annotate(
+            transactions_count=Count('transactions__id'),
+            balance=Coalesce(positive_transactions, 0) - Coalesce(negative_transactions, 0) 
+        )
+        return users
+
+    @classmethod
+    def get_total_balance(cls):
+        queryset = cls.get_report()
+        return queryset.aggregate(Sum('balance'))
 
 
 class UserBalance(models.Model):
@@ -30,4 +56,4 @@ class UserBalance(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} - {self.balance} - {self.created_time}"
+        return f"{self.user} - {self.balance} - {self.created_time}"
