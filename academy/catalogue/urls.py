dfea9d6e1297467a53bd7dd8ee7f9fb3794be863
miyankouh <majid.miyankouh@gmail.com>
2022-07-01 15:43:26 +0430

    add: property to models / product-search to urls / product_search to views

--- academy/catalogue/urls.py
+++ academy/catalogue/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import product_list, product_detail, category_products
+from .views import product_list, product_detail, category_products, product_search
+
 
 urlpatterns = [
     path('product/list', product_list, name='product-list'),
+    path('product/search/', product_search, name='product-search'),
     path('product/detail/<int:pk>/', product_detail, name='product-detail'),
     path('category/<int:pk>/products/', category_products, name='product-detail'),
 ]
--- academy/catalogue/views.py
+++ academy/catalogue/views.py
@@ -6,7 +6,7 @@ from catalogue.models import Brand, Category, Product, ProductType
 def product_list(request):
     # products = Product.objects.filter(is_active=True)
     # products = Product.objects.exclude(is_active=False)
-    
+
     # category = Category.objects.first()
     # category = Category.objects.last()
 
@@ -18,14 +18,15 @@ def product_list(request):
 
     # brand = Brand.objects.filter()
     # product_type = ProductType.objects.filter(title="Book")
-    
+
     # new_product = Product.objects.create(
     #     product_type = product_type, upc=789465, title="test Product",
     #     description='', category=category, brand=brand
     # )
 
     products = Product.objects.select_related('category').all()
-    context = "\n".join([f"{product.title}, {product.upc}, {product.category.name}" for product in products])
+    context = "\n".join(
+        [f"{product.title}, {product.upc}, {product.category.name}" for product in products])
     return HttpResponse(context)
 
 
@@ -38,7 +39,8 @@ def product_detail(request, pk):
     #     except Product.DoesNotExist:
     #         return HttpResponse("PRODUCT DOES NOT EXIST")
 
-    queryset = Product.objects.filter(is_active=True).filter(Q(pk=pk) | Q(upc=pk))
+    queryset = Product.objects.filter(
+        is_active=True).filter(Q(pk=pk) | Q(upc=pk))
     if queryset.exists():
         product = queryset.first()
         return HttpResponse(f"title: {product.title}")
@@ -54,5 +56,19 @@ def category_products(request, pk):
 
     # products = Product.objects.filter(category=category)
 
-    context = "\n".join([f"{product.title}, {product.upc}" for product in products])
+    context = "\n".join(
+        [f"{product.title}, {product.upc}" for product in products])
     return HttpResponse(context)
+
+
+def product_search(request):
+    title = request.GET.get('q')
+
+    products = Product.objects.filter(
+        is_active=True, title__icontains=title, category__name__icontains=title, category__is_active=True
+    )
+    products = Product.objects.filter(is_active=True).filter(title__icontains=title).filter(
+        category__name__icontains=title).filter(category__is_active=True).distinct()
+    context = "\n".join(
+        [f"{product.title}, {product.upc}" for product in products])
+    return HttpResponse(f"search page :\n{context}")
