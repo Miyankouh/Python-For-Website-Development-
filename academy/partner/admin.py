366d97d13c5eceacf6868fd266b8980c33252844
miyankouh <majid.miyankouh@gmail.com>
2022-07-04 14:15:19 +0430

    register Partner & PartnerStock

--- academy/partner/admin.py
+++ academy/partner/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from partner.models import Partner, PartnerStock
+
+
+@register(PartnerStock)
+class PartnerStockAdmin(admin.ModelAdmin):
+    pass
+
+
+@register(Partner)
+class ParentAdmin(admin.ModelAdmin):
+    pass
