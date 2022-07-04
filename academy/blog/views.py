6039bd13fdcc27c05fbf339f86b48d49ab48855b
miyankouh <majid.miyankouh@gmail.com>
2022-07-04 14:11:08 +0430

    change: HttpResponse

--- academy/blog/views.py
+++ academy/blog/views.py
@@ -1,22 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 
 def post_list(request, year=None, month=None):
     if month is not None:
         return HttpResponse(f'post list archive for {year} and {month}')
-        
+
     if year is not None:
         return HttpResponse(f'post list archive for {year}')
-    
-    
-    return HttpResponse('posts list page')
-    
+
+    return HttpResponse('<h2>posts list page</h2></br><h2>posts list page</h2>')
+
 
 def category_list(request):
     return HttpResponse('category list page')
-    
+
 
 def post_detail(request, post_title):
     return HttpResponse(f'post detail {post_title}')
