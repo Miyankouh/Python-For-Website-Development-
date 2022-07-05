c994f8d312a94236fc8a8a9d572d9e38d6a936a4
miyankouh <majid.miyankouh@gmail.com>
2022-07-05 17:17:51 +0430

    add: product image inline & product attribute value

--- academy/catalogue/admin.py
+++ academy/catalogue/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import register
-from catalogue.models import Category, Brand, Product, ProductAttribute, ProductType
+from catalogue.models import Category, Brand, Product, ProductAttribute, ProductAttributeValue, ProductImage, ProductType
 
 
 class ProductAttributeInline(admin.TabularInline):
@@ -8,14 +8,25 @@ class ProductAttributeInline(admin.TabularInline):
     extra = 1
 
 
+class ProductAttributeValue(admin.TabularInline):
+    model = ProductAttributeValue
+    extra = 1
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
 @register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('upc', 'product_type', 'title', 'is_active', 'category', 'brand')
+    list_display = ('upc', 'product_type', 'title',
+                    'is_active', 'category', 'brand')
     list_display_links = ('title',)
     list_filter = ('is_active',)
     list_editable = ('is_active',)
     search_fields = ('upc', 'title', 'category__name', 'brand__name')
     actions = ('active_all',)
+    inlines = [ProductAttributeValue, ProductImageInline]
 
     def active_all(self, request, queryset):
         pass
@@ -31,7 +42,7 @@ class ProductTypeAdmin(admin.ModelAdmin):
 
 
 class ProductAttributeAdmin(admin.ModelAdmin):
-    list_display = ['title','product_type', 'attribute_type']
+    list_display = ['title', 'product_type', 'attribute_type']
 
 
 admin.site.register(Category)
