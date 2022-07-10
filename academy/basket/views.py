6746e998f7bf02738f89002443786dbac168beea
miyankouh <majid.miyankouh@gmail.com>
2022-07-10 17:34:45 +0430

    change: add_to_basket

--- academy/basket/views.py
+++ academy/basket/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
+from basket.forms import AddToBasketForm
 from catalogue.models import Product
 from basket.models import Basket
 
@@ -19,35 +20,17 @@ def add_to_basket(request):
     
     response = HttpResponseRedirect(request.POST.get('next', '/'))
 
-    basket_id = request.COOKIES.get('basket_id', None)
-    if basket_id is None:
-        basket = Basket.objects.create()
-        response.set_cookie('basket_id', basket.id)
-    else:
-        try:
-            basket = Basket.objects.get(pk=basket_id)
-        except Basket.DoesNotExist:
-            raise Http404
+    basket = Basket.get_basket(request.COOKIES.get('basket_id', None))
+    if basket is None:
+        raise Http404
 
-    if request.user.is_authenticated:
-        if basket.user is not None and basket.user != request.user:
-            raise Http404
-        basket.user = request.user
-        basket.save()
+    response.set_cookie('basket_id', basket.id)
 
-    product_id = request.POST.get('product_id', None)
-    quantity = request.POST.get('quantity', 1)
-    try:
-        quantity = int(quantity)
-    except:
-        quantity = 1
+    if not basket.validate_user(request.user):
+        raise Http404
 
-    if product_id is not None:
-        try:
-            product = Product.objects.get(pk=product_id)
-        except Product.DoesNotExist:
-            raise Http404
-        else:
-            basket.add(product, quantity)
+    form = AddToBasketForm(request.POST)
+    if form.is_valid():
+        form.save(basket=basket)
 
     return response
