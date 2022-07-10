32c6628bfcde08a5594ffda899253ac5f15dabb9
miyankouh <majid.miyankouh@gmail.com>
2022-07-10 11:51:39 +0430

    reg: register basket models

--- academy/basket/admin.py
+++ academy/basket/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from basket.models import Basket, BasketLine
 # Register your models here.
+
+
+class BasketInline(admin.TabularInline):
+    model = BasketLine
+
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_time']
+    inlines = (BasketInline, )
+
+
+admin.site.register(Basket, BasketAdmin)
