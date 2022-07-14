6983b9bd6e5b49e14de0d67ff33989af14b4c604
miyankouh <majid.miyankouh@gmail.com>
2022-07-14 12:27:33 +0430

    add: AddressListView

--- academy/shipping/urls.py
+++ academy/shipping/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from shipping.views import address_create, address_list
+from shipping.views import AddressListView, address_create, address_list
 
 
 urlpatterns = [
-    path("list/", address_list, name="address-list"),
+    path("list/", AddressListView.as_view(), name="address-list"),
+    # path("list/", address_list, name="address-list"),
     path("create/", address_create, name="address-create"),
 ]
