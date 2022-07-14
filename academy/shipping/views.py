71845ce272a4e41db87dce6023f22f87d12dcc34
miyankouh <majid.miyankouh@gmail.com>
2022-07-14 12:28:51 +0430

    add: AddressListView , CustomUserListView

--- academy/shipping/views.py
+++ academy/shipping/views.py
@@ -1,12 +1,38 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
-
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.decorators.http import require_http_methods, require_GET
+from shipping.models import ShippingAddress
 from shipping.forms import SippingAddressForm
 
+
+@login_required
+@require_GET
 def address_list(request):
-    return HttpResponse("Address list")
+    queryset = ShippingAddress.objects.filter(user=request.user)
+    return render(request, 'shipping/list.html', {'queryset': queryset})
+
+
+class CustomUserListView(ListView):
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+         return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
+
+
+class AddressListView(CustomUserListView):
+        model = ShippingAddress
+
+        # @method_decorator(login_required)
+    # def get(self, request):
+    #     queryset = ShippingAddress.objects.filter(user=request.user)
+    #     return render(request, 'shipping/list.html', {'queryset': queryset})
 
 
 @login_required
