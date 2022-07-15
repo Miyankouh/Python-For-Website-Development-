b24e32eadf10acae2e24f7f2f66f14e78d1d392b
miyankouh <majid.miyankouh@gmail.com>
2022-07-15 12:23:25 +0430

    add: FormView

--- academy/shipping/views.py
+++ academy/shipping/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
 from django.views.decorators.http import require_http_methods, require_GET
 from shipping.models import ShippingAddress
 from shipping.forms import SippingAddressForm
@@ -29,6 +31,10 @@ class CustomUserListView(ListView):
 class AddressListView(CustomUserListView):
         model = ShippingAddress
 
+        def get_context_data(self,*args, object_list=None, **kwargs):
+            context = super().get_context_data(*args, object_list=object_list, **kwargs)
+            context['extra_data'] = self.get_queryset().count()
+            return context
         # @method_decorator(login_required)
     # def get(self, request):
     #     queryset = ShippingAddress.objects.filter(user=request.user)
@@ -49,3 +55,16 @@ def address_create(request):
     else:
         form = SippingAddressForm()
     return render(request, 'shipping/create.html', {'form': form})
+
+
+class AddressCreateView(FormView):
+    form_class = SippingAddressForm
+    template_name = 'shipping/create.html'
+    success_url = reverse_lazy('address-list')
+    # success_url = '/shipping/list/'
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        instance.save()
+        return super().form_valid(form)
