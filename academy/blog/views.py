fe130ec63e0c146d5576e76b45c11f4c2b654fd9
miyankouh <majid.miyankouh@gmail.com>
2022-07-15 12:20:53 +0430

    use class base view

--- academy/blog/views.py
+++ academy/blog/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView 
+from blog.models import Post
+
 
 def post_list(request, year=None, month=None):
     if month is not None:
@@ -12,12 +16,21 @@ def post_list(request, year=None, month=None):
     return HttpResponse('<h2>posts list page</h2></br><h2>posts list page</h2>')
 
 
+class BlogListView(ListView):
+    model = Post
+    template_name = ".html"
+
+
 def category_list(request):
     return HttpResponse('category list page')
 
 
 def post_detail(request, post_title):
     return HttpResponse(f'post detail {post_title}')
+
+
+class BlogPostDetailView(DetailView):
+    model = Post
 
 
 def custom_post_detail(request):
