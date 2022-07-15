e83e123e3a9c4c7b5d4d846e40a7c7676bc3a8b8
miyankouh <majid.miyankouh@gmail.com>
2022-07-15 12:22:25 +0430

    change: address_create to AdressCreateView.as_view()

--- academy/shipping/urls.py
+++ academy/shipping/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
-from shipping.views import AddressListView, address_create, address_list
+from shipping.views import AddressCreateView, AddressListView, address_create, address_list
 
 
 urlpatterns = [
     path("list/", AddressListView.as_view(), name="address-list"),
     # path("list/", address_list, name="address-list"),
-    path("create/", address_create, name="address-create"),
+    # path("create/", address_create, name="address-create"),
+    path("create/", AddressCreateView.as_view(), name="address-create"),
 ]
