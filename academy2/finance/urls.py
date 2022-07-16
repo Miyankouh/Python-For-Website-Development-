0f14b0ca8da869271dc34bdd38a522870161f324
miyankouh <majid.miyankouh@gmail.com>
2022-07-16 19:54:46 +0430

    add: Verify

--- academy2/finance/urls.py
+++ academy2/finance/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from finance.views import ChargeWalletView
+from finance.views import ChargeWalletView, VerifyView
 
 
 urlpatterns = [
-    path("charge/", ChargeWalletView.as_view(), name="")
+    path("charge/", ChargeWalletView.as_view(), name=""),
+    path("verify/", VerifyView.as_view(), name=""),
 ]
