2852ba7dc9e29c3357fa1a4e858945fb25fb53e5
miyankouh <majid.miyankouh@gmail.com>
2022-07-16 19:55:03 +0430

    add: VerifyView

--- academy2/finance/views.py
+++ academy2/finance/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.shortcuts import render, redirect
 from django.views import View
 from finance.forms import ChargeWalletForm
-from finance.utils.zarinpal import zpal_request_handler
+from finance.utils.zarinpal import zpal_payment_checker, zpal_request_handler
 
 
 class ChargeWalletView(View):
@@ -24,3 +24,16 @@ class ChargeWalletView(View):
                 return redirect(payment_link) 
 
         return render(request, self.template_name, {'form': form})
+
+
+class VerifyView(View):
+    template_name = 'finance/callback.html'
+
+    def get(self,request, *args, **kwargs):
+        authority = request.GET.get('Authority')
+        is_paid, ref_id = zpal_payment_checker(
+            settings.ZARRINPAL['merchant_id'],
+            1000, # amount
+            authority
+        )
+        return render(request, self.template_name, {'is_paid': is_paid, 'ref_id': ref_id})
