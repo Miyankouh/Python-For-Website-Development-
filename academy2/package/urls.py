f3910c69ae37a975537f6381f0366de3a9b54ac5
miyankouh <majid.miyankouh@gmail.com>
2022-07-17 16:15:28 +0430

    use: TemplateView in urls

---
+++ academy2/package/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('pricing/', TemplateView.as_view(template_name='package/pricing_page.html'))
+]
