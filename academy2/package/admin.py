e33b16ad39d47470fbb4b5e493cce783267f3b4d
miyankouh <majid.miyankouh@gmail.com>
2022-07-18 15:41:18 +0430

    register: PackageModelAdmin & PackageAttributeInline

--- academy2/package/admin.py
+++ academy2/package/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import register
+
+from package.models import Package, PackageAttribute
 
 # Register your models here.
+
+class PackageAttributeInline(admin.TabularInline):
+    model = PackageAttribute
+
+
+@register(Package)
+class PackageModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+    inlines = (PackageAttributeInline,)
