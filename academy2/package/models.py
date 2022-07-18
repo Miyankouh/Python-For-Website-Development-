2f48d3cdd04278107138f40c69411e372fb1cf0b
miyankouh <majid.miyankouh@gmail.com>
2022-07-18 15:43:14 +0430

    add: Package

--- academy2/package/models.py
+++ academy2/package/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Package(models.Model):
+    title = models.CharField(max_length=48)
+    price = models.PositiveBigIntegerField()
+    description = models.TextField(blank=True)
+    days = models.PositiveSmallIntegerField()
+    is_enable = models.BooleanField(default=True)
+
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class PackageAttribute(models.Model):
+    package = models.ForeignKey(Package, related_name='attributes', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+
+    def __str__(self) :
+        return  self.title
