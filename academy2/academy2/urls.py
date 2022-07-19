5f0bd8225c787cdc4c7800e6387bdbd1fa8b02d4
miyankouh <majid.miyankouh@gmail.com>
2022-07-19 16:19:27 +0430

    add: purchase urls

--- academy2/academy2/urls.py
+++ academy2/academy2/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('finance/', include('finance.urls')),
     path('package/', include('package.urls')),
+    path('purchase/', include('purchase.urls')),
 ]
